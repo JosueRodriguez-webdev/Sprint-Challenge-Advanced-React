03ec749dbb7114cd2fd3cc5081a8f6780f58ddd4
Josue Rodriguez <jr2589213@gmail.com>
2020-02-14 08:24:28 -0800

    Fetch data in App.js for Players (http://localhost:5000/api/players)
    -Switched app.js from a function components to a class component
    
    -imported axios and did a axios.get inside the componentdidmount in order to fetch data
    
    -set state to the data of players

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import axios from 'axios'
+
+class App extends React.Component {
+constructor(){
+  super()
+  this.state = {
+    playersData: []
+  }
+}
+
+componentDidMount() {
+  axios.get(`http://localhost:5000/api/players`)
+  .then(res => {
+    console.log(`Success! you got data in you app.js`,res.data)
+    this.setState({playersData: res.data})
+  })
+  .catch(err => {
+    console.log(`Error, try again in your app.js`,err)
+  })
+}
+
+
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
     </div>
   );
+}
 }
 
 export default App;
